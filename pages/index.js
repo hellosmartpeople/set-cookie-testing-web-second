655cd5d5fef577250dfbfd1fa03b33231e37b4d8
alfisyahri313 <alfi@ilmuonedata.com>
2022-11-02 17:44:18 +0700

    duplicate flow to second website

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { setCookie, getCookie} from 'cookies-next';
+
+
+export const getServerSideProps = (context) => {
+  const req = context.req
+  const res = context.res
+  const userCookie = getCookie('iodUsers', { req, res });
+  const host = context.req.headers.host
+  const { query } = context
+
+  if (!userCookie && !('_gl' in query)) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: 'https://redirection-testing-web.herokuapp.com/?referrer='+host,
+      },
+    };
+  }
+
+  setCookie('iodUsers', 'true', { req, res, maxAge: 60 * 60 * 24 * 30, httpOnly: true});
+
+  return { props: {} };
+};
 
 export default function Home() {
   return (
@@ -8,10 +31,18 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-TD263X2");`}
+        </script>
+
+        <script id="analyticsjs" strategy="afterInteractive">
+        {`(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "G-004ZVSZ5S4", "auto");`}
+        </script> 
       </Head>
 
       <main>
-        <Header title="Welcome to web 2!" />
+        <Header title="Welcome to my web 2!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
